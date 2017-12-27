011fa2280ef1c37bd57c7f7b217dff966f6c7eda
Nikolay Govorov <nikolay_govorov@bk.ru>
2017-12-27 14:59:40 +0300

    Writed simple generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,42 @@
-use std::borrow::Cow;
+extern crate rand;
 
-static URL_SYMBOLS : &'static str =
-    "_~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+use rand::Rng;
 
-pub extern fn generate<'a>() -> Cow<'a, str> {
-    let input : &'a str = URL_SYMBOLS;
+const URL_SYMBOLS: [char; 64] = [
+    '_', '~', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
 
-    return input.into();
+fn random(size: usize) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+
+    let mut result: Vec<u32> = vec![0; size];
+
+    for i in 0..size {
+        result[i] = rng.gen::<u32>();
+    }
+
+    result
 }
 
 #[test]
-fn it_works() {
-    assert_eq!(generate(), URL_SYMBOLS);
+fn test_random() {
+    let bytes : Vec<u32> = random(5);
+
+    assert_eq!(bytes.len(), 5);
+}
+
+pub extern fn simple(size: usize) -> String {
+    let mut id = String::new();
+
+    let bytes = random(size);
+
+    for i in 0..size {
+        let index = bytes[i] & 63;
+
+        id.push(URL_SYMBOLS[index as usize]);
+    }
+
+    id
 }
