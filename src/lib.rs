7b7140bf2ab55aea32880d5889f653c4593cefcc
Ariarzer <ariarzer@gmail.com>
2017-12-30 22:44:13 +0300

    Change the method of recording number of test-ID

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@ mod simple {
     /// Has no collisions
     #[test]
     fn no_collisions () {
-        let count = 100 * 1000;
+        let count = 10^5;
 
         let mut ids = HashMap::new();
 
