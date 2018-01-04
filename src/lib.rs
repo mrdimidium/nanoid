9b95a22f5b46d01a2007c6c8f99bd0c0cea05899
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-01-04 07:43:32 +0300

    Fixed require SAFE_ALPHABET

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate rand;
 
 use rand::Rng;
 
-const SAFE_ALPHABET: [char; 64] = [
+pub const SAFE_ALPHABET: [char; 64] = [
     '_', '~',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
