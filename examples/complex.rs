33d3b42ce727211477ba3b949ca229ff6715af45
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-01-06 23:07:37 +0300

    Updated the complex example

--- examples/complex.rs
+++ examples/complex.rs
@@ -3,17 +3,17 @@ extern crate nanoid;
 
 use rand::Rng;
 
-fn main() {
-    fn random (size: usize) -> Vec<u32> {
-        let mut rng = rand::thread_rng();
-        let mut bytes: Vec<u32> = vec![0; size];
+fn random (size: usize) -> Vec<u32> {
+    let mut rng = rand::thread_rng();
+    let mut bytes: Vec<u32> = vec![0; size];
 
-        for i in 0..size {
-            bytes[i] = rng.gen::<u32>();
-        }
-
-        bytes
+    for i in 0..size {
+        bytes[i] = rng.gen::<u32>();
     }
 
+    bytes
+}
+
+fn main() {
     nanoid::complex(10, &['a', 'b', 'c', 'd', 'e', 'f'], random); //=> "fbaefaadeb"
 }
