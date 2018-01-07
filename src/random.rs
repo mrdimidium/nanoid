bba0c4bb6a8cfd1beecd438001c35f528add5604
Nikolay Govorov <nikolay_govorov@bk.ru>
2018-01-07 05:37:21 +0300

    Add OS generator

--- src/random.rs
+++ src/random.rs
@@ -1,6 +1,7 @@
 use rand;
 
 use rand::Rng;
+use rand::os::OsRng;
 
 pub fn standart(size: usize) -> Vec<u32> {
     let mut rng = rand::thread_rng();
@@ -15,12 +16,34 @@ pub fn standart(size: usize) -> Vec<u32> {
 }
 
 #[cfg(test)]
-mod test_random {
+mod test_standart {
     use super::*;
 
     #[test]
     fn generates_random_vectors() {
         let bytes : Vec<u32> = standart(5);
+
+        assert_eq!(bytes.len(), 5);
+    }
+}
+
+pub fn os(size: usize) -> Vec<u32> {
+    let mut random = OsRng::new().unwrap();
+    let mut result: Vec<u32> = vec![0; size];
+
+    for i in 0..size {
+        result[i] = random.next_u32();
+    }
+
+    result
+}
+
+mod test_secure {
+    use super::*;
+
+    #[test]
+    fn generates_random_vectors() {
+        let bytes: Vec<u32> = os(5);
 
         assert_eq!(bytes.len(), 5);
     }
