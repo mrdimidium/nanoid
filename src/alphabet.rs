f0ad07fc16b96b4c00d76fa10853fa377ad8ee05
Nikolay Govorov <me@govorov.online>
2026-04-13 19:02:19 +0100

    #39: Add hex alphabets

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -14,3 +14,25 @@ pub const SAFE: [char; 64] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
+
+/// Lowercase hexadecimal alphabet (`0-9`, `a-f`).
+///
+/// # Example
+///
+/// ```
+/// let id = nanoid::nanoid!(10, &nanoid::alphabet::HEX_LOWERCASE);
+/// ```
+pub const HEX_LOWERCASE: [char; 16] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+];
+
+/// Uppercase hexadecimal alphabet (`0-9`, `A-F`).
+///
+/// # Example
+///
+/// ```
+/// let id = nanoid::nanoid!(10, &nanoid::alphabet::HEX_UPPERCASE);
+/// ```
+pub const HEX_UPPERCASE: [char; 16] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+];
